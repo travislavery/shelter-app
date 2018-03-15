16e6268e04dcf74faa8ea22cb4f2c8a90d1bfdfd
TravisA <github email address>
2018-03-14 20:31:42 -0600

    add component for social media links

---
+++ src/components/SocialMediaLinks.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {Button} from 'react-bootstrap'
+
+const SocialMediaLinks = ({shelter}) => {
+	return (
+		<div id="social-media-links" className="btn-group-vertical">
+			<Button>Hello</Button>
+			<Button>Hello</Button>
+			<Button>Hello</Button>
+		</div>
+	)
+}
+
+export default SocialMediaLinks
