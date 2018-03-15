e4ddd4dc5d3447b49d241410bde9b4d72b4d2738
TravisA <github email address>
2018-03-15 13:10:32 -0600

    move form to ShelterForm component

---
+++ src/components/shelters/ShelterForm.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import {Form, Button} from 'react-bootstrap'
+
+const ShelterForm = (props) => {
+	return (
+		<Form onSubmit={(event)=> props.handleOnSubmit(event)}>
+			<div className="form-group form-row">
+				<label className="col-sm-2 col-form-label">Shelter Name: </label>
+				<div className="col-sm-10">
+	          <input
+	          	className="form-control"
+	            type="text"
+	            placeholder="Shelter Name"
+	            name="name"
+	            onChange={(event)=> props.handleOnChange(event)} />
+	    	</div>
+      </div>
+      <div className="form-group form-row">
+        <label className="col-sm-2 col-form-label">Address: </label>
+      	<div className="col-sm-10">
+        	<input
+          	className="form-control"
+            type="text"
+            placeholder="Address"
+            name="address"
+            onChange={(event)=> props.handleOnChange(event)} />
+      	</div>
+      </div>
+      <div className="form-group form-row">
+      	<label className="col-sm-2 col-form-label">Phone Number: </label>
+     		<div className="col-sm-10">
+          <input
+          	className="form-control"
+            type="tel"
+            placeholder="Phone Number"
+            name="phone_number"
+            onChange={(event)=> props.handleOnChange(event)} />
+      	</div>
+      </div>
+      <div className="form-group form-row">
+      	<label className="col-sm-2 col-form-label">Picture Url: </label>
+     		<div className="col-sm-10">
+          <input
+          	className="form-control"
+            type="text"
+            placeholder="Image URL"
+            name="image"
+            onChange={(event)=> props.handleOnChange(event)} />
+      	</div>
+      </div>
+      <Button
+        type="submit"
+        className="btn btn-primary">
+      	Create Shelter
+      </Button>    
+    </Form>
+	)
+}
+
+export default ShelterForm
+
--- src/containers/shelters/ShelterNew.js
+++ src/containers/shelters/ShelterNew.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {createShelter} from '../../actions/shelters'
-import {Form, Button} from 'react-bootstrap'
+import ShelterForm from '../../components/shelters/ShelterForm'
 
 class ShelterNew extends Component {
 	constructor(props) {
@@ -37,58 +37,7 @@ class ShelterNew extends Component {
 		return (
 			<div>
 				<h1>Create a new shelter</h1>
-				<Form onSubmit={this.handleOnSubmit}>
-					<div className="form-group form-row">
-						<label className="col-sm-2 col-form-label">Shelter Name: </label>
-						<div className="col-sm-10">
-		          <input
-		          	className="form-control"
-		            type="text"
-		            placeholder="Shelter Name"
-		            name="name"
-		            onChange={this.handleOnChange} />
-	        	</div>
-	        </div>
-	        <div className="form-group form-row">
-		        <label className="col-sm-2 col-form-label">Address: </label>
-		      	<div className="col-sm-10">
-	          	<input
-		          	className="form-control"
-		            type="text"
-		            placeholder="Address"
-		            name="address"
-		            onChange={this.handleOnChange} />
-          	</div>
-          </div>
-          <div className="form-group form-row">
-          	<label className="col-sm-2 col-form-label">Phone Number: </label>
-         		<div className="col-sm-10">
-		          <input
-		          	className="form-control"
-		            type="tel"
-		            placeholder="Phone Number"
-		            name="phone_number"
-		            onChange={this.handleOnChange} />
-          	</div>
-          </div>
-          <div className="form-group form-row">
-          	<label className="col-sm-2 col-form-label">Picture Url: </label>
-         		<div className="col-sm-10">
-		          <input
-		          	className="form-control"
-		            type="text"
-		            placeholder="Image URL"
-		            name="image"
-		            onChange={this.handleOnChange} />
-          	</div>
-          </div>
-          <Button
-            type="submit"
-            className="btn btn-primary">
-          	Create Shelter
-          </Button>
-          
-        </Form>
+				<ShelterForm handleOnChange={this.handleOnChange} handleOnSubmit={this.handleOnSubmit}/>
 			</div>
 		)
 	}
