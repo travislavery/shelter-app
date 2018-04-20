5b9633802253b5f881b10fbf91161955a36b6c35
TravisA <github email address>
2018-04-19 18:58:18 -0600

    add itemGroupReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import sheltersReducer from './sheltersReducer';
 import userReducer from './userReducer'
 import itemsReducer from './itemsReducer'
 import errorsReducer from './errorsReducer'
+import itemGroupsReducer.js from './itemGroupsReducer.js'
 
 export default combineReducers({
   shelters: sheltersReducer,
   user: userReducer,
   items: itemsReducer,
   errors: errorsReducer,
+  item_groups: itemGroupsReducer,
 });
