4f0e1204130afdc0c8613660274acf6b0fb8ad3a
TravisA <github email address>
2018-04-22 20:51:19 -0600

    add header title and well

--- src/components/item_group/ItemGroupSection.js
+++ src/components/item_group/ItemGroupSection.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import SubGroupTable from './SubGroupTable'
+import {Well} from 'react-bootstrap'
 
 const ItemGroupSection = (props) => {
 	const subGroupTables = props.item_group.items.map(item => <SubGroupTable key={item.id} item={item}/>)
 	return (
-		<div>
+		<Well bsSize="large" bsClass="item-group-well">
 			<h1>{props.item_group.name}</h1>
 			{subGroupTables}
-		</div>
+		</Well>
 	)
 }
 
